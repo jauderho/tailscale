f8e21c0ad101870e28f4f46e55352dff6da162ae
David Anderson <danderson@tailscale.com>
2021-06-27 02:05:21 -0700

    WIP: noise protocol
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- control/noise/noise.go
+++ control/noise/noise.go
@@ -4,12 +4,14 @@
 package noise
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 
-	"github.com/aead/chacha20poly1305"
+	chp "github.com/aead/chacha20poly1305"
 	"golang.org/x/crypto/blake2s"
 	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/hkdf"
@@ -23,6 +25,86 @@ const (
 	encryptedKeySize = 32 + 8 // ciphertext + poly1305 tag
 	protocolName     = "Noise_IKpsk1_25519_ChaChaPoly_BLAKE2s"
 )
+
+type symmetricState struct {
+	h            [blake2s.Size]byte
+	ck           [blake2s.Size]byte
+	k            [chp.KeySize]byte
+	kInitialized bool
+	n            uint64
+}
+
+func (s *symmetricState) Initialize() {
+	s.h = blake2s.Sum(protocolName)
+	s.ck = h
+	s.k = [chp.KeySize]byte{}
+	s.kInitialized = false
+	s.n = 0
+}
+
+func (s *symmetricState) MixKey(keyData []byte) {
+	r := hkdf.New(blake2s.New, keyData, s.ck[:], nil)
+	if _, err := io.ReadFull(r, s.ck[:]); err != nil {
+		// HKDF only fails to read if we try to read 256 keys out. We
+		// only read out 2.
+		panic(fmt.Sprintf("HKDF failed: %v", err))
+	}
+	if _, err := io.ReadFull(r, s.k[:]); err != nil {
+		panic(fmt.Sprintf("HKDF failed: %v", err))
+	}
+	s.kInitialized = true
+	s.n = 0
+}
+
+func (s *symmetricState) MixKeyAndHash(keyData []byte) {
+	r := hkdf.New(blake2s.New, keyData, s.ck[:], nil)
+	if _, err := io.ReadFull(r, s.ck[:]); err != nil {
+		// HKDF only fails to read if we try to read 256 keys out. We
+		// only read out 2.
+		panic(fmt.Sprintf("HKDF failed: %v", err))
+	}
+	var temph [blake2s.KeySize]byte
+	if _, err := io.ReadFull(r, temph[:]); err != nil {
+		panic(fmt.Sprintf("HKDF failed: %v", err))
+	}
+	st.MixHash(temph[:])
+	if _, err := io.ReadFull(r, s.k[:]); err != nil {
+		panic(fmt.Sprintf("HKDF failed: %v", err))
+	}
+	s.kInitialized = true
+	s.n = 0
+}
+
+// MixDH is MixKey(X25519(sec, pub))
+func (s *symmetricState) MixDH(sec key.Private, pub key.Public) error {
+	km, err := curve25519.X25519(sec[:], pub[:])
+	if err != nil {
+		return err
+	}
+	s.MixKey(km)
+}
+
+func (s *symmetricState) MixHash(data []byte) {
+	h := blake2s.New()
+	h.Write(s.h[:])
+	h.Write(data)
+	h.Sum(s.h[:0])
+}
+
+func (s *symmetricState) EncryptAndHash(plaintext []byte) []byte {
+	if !s.kInitialized {
+		s.MixHash(plaintext)
+		return plaintext
+	}
+
+	aead := chp.New(s.k[:])
+	var nonce [chp.NonceSize]byte
+	binary.BigEndian.PutUint64(nonce[4:], s.n)
+	s.n++
+	ret := aead.Seal(nil, nonce[:], plaintext, s.h[:])
+	st.MixHash(ret)
+	return ret
+}
 
 // Initiate runs the IKpsk1 handshake from the initiator's perspective
 // (i.e. the client talking to Control). If the handshake is
@@ -40,49 +122,33 @@ func Initiate(ctx context.Context, conn net.Conn, machineKey key.Private, contro
 		e   key.Private
 		re  key.Public
 		psk = ratchet
-		h   = blake2s.Sum256([]byte(protocolName))
-		ck  = h
-		k   [chacha20poly1305.KeySize]byte
-		n   uint64
+		st  symmetricState
 	)
 
-	mixHash := func(data []byte) {
-		hh := blake2s.New()
-		hh.Write(h[:])
-		hh.Write(data)
-		hh.Sum(h[:0])
-	}
-	mixKey := func(keyData []byte) {
-		r := hkdf.New(blake2s.New, keyData, ck[:])
-		if _, err := io.ReadFull(r, ck[:]); err != nil {
-			// This can only happen if we exhaust HKDF's entropy,
-			// which would take reading out 256 keys. We only read out
-			// 2.
-			panic(fmt.Sprintf("HKDF failed: %v", err))
-		}
-		if _, err := io.ReadFull(r, k[:]); err != nil {
-			panic(fmt.Sprintf("HKDF failed: %v", err))
-		}
-		n = 0
-	}
-
-	mixHash(nil) // empty prologue, but we have to mix.
+	st.Initialize()
+	st.MixHash(nil) // empty prologue, but we have to mix.
 
 	// <- s
 	// ...
-	mixHash(rs[:])
+	st.MixHash(rs[:])
 
+	var msg bytes.Buffer
 	// -> e, es, s, ss, psk
 	e = key.NewPrivate()
-	copy(msg[:], e.Public()[:])
-	mixHash(e.Public()[:])
+	msg.Write(e.Public()[:])
+	st.MixHash(e.Public()[:])
 
-	es, err := curve25519.X25519(e[:], rs[:])
-	if err != nil {
+	if err := st.MixDH(e, rs); err != nil {
 		return nil, fmt.Errorf("computing es: %w", err)
 	}
-}
 
-func hkdf2(ck, km []byte) ([blake2s.Size]byte, [blake2s.Size]byte) {
+	msg.Write(st.EncryptAndHash(s[:]))
 
+	if err := st.MixDH(s, rs); err != nil {
+		return nil, fmt.Errorf("computing ss: %w", err)
+	}
+
+	st.MixKeyAndHash(psk[:])
+
+	return nil, nil
 }
